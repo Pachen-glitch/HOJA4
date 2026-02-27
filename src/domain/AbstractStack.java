06ea7ed8d68943efeec5d5c6eaee9a60d37a2433
Jorge <jjmarcant06@gmail.com>
2026-02-27 09:30:04 -0600

    STAQCKS

---
+++ src/domain/AbstractStack.java
@@ -0,0 +1,16 @@
+package domain;
+
+public abstract class AbstractStack<T> implements Stack<T> {
+
+    protected int count;
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public int size() {
+        return count;
+    }
+}
